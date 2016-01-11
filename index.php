8747da072c6d6ea6eb5f7130ea49bf0a80c61a30
Rafael Abreu <rafael.abreu@taghos.com.br>
2016-01-11 15:54:27 -0200

    Exemplo de uso
    Criado exemplo de uso de uma transação e um estorno com as novas classes.

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <?php
-include "komerci.php";
-use komerci;
+include './komerci.php';
+include "./user-komerci.php";
 
-komerci\config("filiacao", "012345678");
-komerci\config("senha", "minha senha secreta");
+use komerci\UserKomerci;
 
-print_r(komerci\comprovante("123456789", "123456", "20121031"));
-?>
+// Exemplo de uso para ambiente de teste
+// Colocar número da filiação válido
+$komerci = new UserKomerci("047506289","testews","testews",true,true,false);
+
+$paga_direto = $komerci->paga_direto("1234", "0.01", "00", "4539372053217316", "517", "10", "2017", "Bla");
+var_dump($paga_direto);
+
+echo "<br />";
+echo "<br />";
+echo "<br />";
+
+$estorna = $komerci->estorna($paga_direto['NUMCV'], $paga_direto['NUMAUTOR'], '0.01');
+var_dump($estorna);
